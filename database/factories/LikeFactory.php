c37856f81afaaaf2e7aac8ec6e389baa2287dfad
bukela <bukela@gmail.com>
2018-06-12 11:49:17 +0200

    db seeder

--- database/factories/LikeFactory.php
+++ database/factories/LikeFactory.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\User;
+use App\Model\Like;
 use Faker\Generator as Faker;
 
 $factory->define(App\Model\Like::class, function (Faker $faker) {
     return [
-        //
+        'user_id' => function() {
+            return User::all()->random();
+        }
     ];
 });
--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -1,9 +1,20 @@
 <?php
 
+use App\User;
+use App\Model\Category;
 use Faker\Generator as Faker;
 
 $factory->define(App\Model\Question::class, function (Faker $faker) {
+    $title = $faker->sentence(); // definise se kao var da bi mogao da se odradi slug
     return [
-        //
+        'title' => $title,
+        'slug' => str_slug($title),
+        'body' => $faker->text,
+        'category_id' => function() {
+            return Category::all()->random(); // ide kroz funkciju da bi pokupio realne id-jeve, a kod user_id isto tako
+        },
+        'user_id' => function() {
+            return User::all()->random();
+        }
     ];
 });
