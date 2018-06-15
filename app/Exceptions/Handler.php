08437a89a7dc68c8219545234bb4c2e075e515b6
bukela <bukela@gmail.com>
2018-06-15 12:25:59 +0200

    jwt tokens

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,10 @@
 namespace App\Exceptions;
 
 use Exception;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Tymon\JWTAuth\Exceptions\TokenBlacklistedException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -46,6 +50,18 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if($exception instanceof TokenBlacklistedException) {
+            return response(['error' => 'Token blacklisted !'], 500);
+        }
+        elseif($exception instanceof TokenInvalidException) {
+            return response(['error' => 'Token invalid !'], 500);
+        }
+        elseif($exception instanceof TokenExpiredException) {
+            return response(['error' => 'Token expired !'], 500);
+        }
+        elseif($exception instanceof JWTException) {
+            return response(['error' => 'No token provided !'], 500);
+        }
         return parent::render($request, $exception);
     }
 }
